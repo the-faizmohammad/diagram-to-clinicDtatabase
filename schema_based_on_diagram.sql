16dfef9d9aa1cfedfdaa789dc8f62b09de339a30
Brighton <mtandibrighton0102@gmail.com>
2023-11-29 16:10:23 +0200

    Create treatment table and invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -18,3 +18,22 @@ CREATE TABLE IF NOT EXISTS patients (
     date_of_birth DATE,
     PRIMARY KEY(id)
 );
+
+-- Create the treatments table
+CREATE TABLE IF NOT EXISTS treatments (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    type VARCHAR(200),
+    name VARCHAR(200),
+    PRIMARY KEY(id)
+);
+
+-- Create the invoices table
+CREATE TABLE IF NOT EXISTS invoices (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL(10, 2),
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_medical_history_id FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
+);
